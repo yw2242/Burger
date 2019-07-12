9f6c88fb3829d64e8dfa27c6e3ef24b591cb3595
Megan Wu <meganwu1994@gmail.com>
2019-07-11 23:08:49 -0500

    all file drafted

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name, devour) VALUES ('Baconburger', false);
+INSERT INTO burgers (name, devour) VALUES ('Cheeseburger', false);
+INSERT INTO burgers (name, devour) VALUES ('Ranch burger', true);
+INSERT INTO burgers (name, devour) VALUES ('Lamb burger', true);
+INSERT INTO burgers (name, devour) VALUES ('Veggie burger', true);
+INSERT INTO burgers (name, devour) VALUES ('Spicy', false);
