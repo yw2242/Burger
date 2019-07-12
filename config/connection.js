379c13530df33e3721b2aff77c03b721d3640218
Megan Wu <meganwu1994@gmail.com>
2019-07-11 23:45:57 -0500

    burger page deployed

--- config/connection.js
+++ config/connection.js
@@ -5,8 +5,8 @@ var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "",
-  database: "cat_db"
+  password: "Shiso*ume9",
+  database: "burger_db"
 });
 
 // Make connection.
